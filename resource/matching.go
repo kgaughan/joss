b360a4dbf4dac96fd11771d53a7147fcd9e624c5
Keith Gaughan <k@stereochro.me>
2026-05-31 12:46:52 +0100

    Safer type lookups

--- resource/matching.go
+++ resource/matching.go
@@ -84,7 +84,7 @@ func (ret *MatchingMap) UnmarshalJSON(data []byte) error {
 		return err
 	}
 
-	typ := reflect.TypeOf(zero)
+	typ := reflect.TypeFor[Matching]()
 	typs := strings.Split(typ.String(), ".")[1]
 	for id, res := range tmp {
 		if res == nil {
@@ -113,7 +113,7 @@ func (ret *MatchingMap) UnmarshalYAML(unmarshal func(v any) error) error {
 		return err
 	}
 
-	typ := reflect.TypeOf(zero)
+	typ := reflect.TypeFor[Matching]()
 	typs := strings.Split(typ.String(), ".")[1]
 	for id, res := range tmp {
 		if res == nil {
